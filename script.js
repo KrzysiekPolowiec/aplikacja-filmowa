9f8da1ea5e70042fa7de252eddf89a30d2999c4e
KrzysiekPolo <magdap95o2@gmail.com>
2023-01-14 10:33:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,49 @@ async function getMovies(url) {
    const data = await res.json()
    showMovies(data.results)
 }
+
+function showMovies(movies) {
+   main.innerHTML = ``
+   movies.forEach((movie) => {
+      const {title, poster_path, vote_average, overview } = movie
+      const movieE1 = document.createElement('div')
+      movieE1.classList.add('movie')
+
+      movieE1.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+         <h3>${title}</h3>
+         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+         </div>
+         <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+            </div>
+      `
+      main.appendChild(movieE1)
+   } )
+}
+
+
+function getClassByRate(vote){
+   if(vote =8){
+      return 'green'
+   }
+   else if(vote >=5){
+      return "orange"
+   }
+   else{
+      return "red"
+   }
+}
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault()
+   const searchTerm = search.value
+   if(searchTerm && searchTerm !== ''){
+      getMovies(SEARCH_API+searchTerm)
+      search.value=''
+   } else{
+      window.location.reload()
+   }
+})
